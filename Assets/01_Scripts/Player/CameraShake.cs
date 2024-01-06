2696af6ab85e7d6b281c3b03e6d9e9e2deaa0a0c
hyechxn <a49918072@gmail.com>
2024-01-07 02:55:11 +0900

    camerashake

---
+++ Assets/01_Scripts/Player/CameraShake.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+
+    [SerializeField]
+    private float m_roughness;      //거칠기 정도
+    [SerializeField]
+    private float m_magnitude;      //움직임 범위
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(Shake(1f));
+        }
+    }
+
+    IEnumerator Shake(float duration)
+    {
+        float halfDuration = duration / 2;
+        float elapsed = 0f;
+        float tick = Random.Range(-10f, 10f);
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime / halfDuration;
+
+            tick += Time.deltaTime * m_roughness;
+            transform.position = new Vector3(
+                Mathf.PerlinNoise(tick, 0) - .5f * m_magnitude * Mathf.PingPong(elapsed, halfDuration),
+                Mathf.PerlinNoise(0, tick) - .5f * m_magnitude * Mathf.PingPong(elapsed, halfDuration));
+
+            yield return null;
+        }
+        transform.position = Vector3.zero;
+    }
+}
